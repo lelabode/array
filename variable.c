ebd6ed79c3dba412ec978ae79bed9f56e91cea50
aurelabode <lelabode@hotmail.com>
2025-12-10 17:46:53 +0100

    swapp

--- variable.c
+++ variable.c
@@ -2,15 +2,15 @@
 # include <string.h>
 
 int main (){
-char x = 'X';
-char y = 'Y';
-char temp;
- temp = x;
- x =y;
- y = temp;
+  char x[15] = "Water";
+  char y[15] = "soda";
+  char temp[15];
+  strcpy(temp, x);
+  strcpy(x,y);
+  strcpy(y, temp);
 
-printf("x = %c\n",x);
-printf("y = %c\n" , y);
+  printf("x = %s\n" , x);
+  printf("y = %s\n", y);
 
 
   return 0;
