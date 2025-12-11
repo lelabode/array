c3f7e9b8cca737a0ed1ba94bafbb5436a36d5d76
aurelabode <lelabode@hotmail.com>
2025-12-11 17:43:48 +0100

    char

---
+++ ex2.c
@@ -0,0 +1,30 @@
+# include <stdio.h>
+void sort(char array[] , int size){
+  for(int i = 0; i < size - 1; i ++){
+       for (int j = 0; j< size - i- 1; j ++){
+         if (array[j] > array[j + 1]){
+            char temp = array[j];
+                 array[j] = array[j + 1];
+                  array [j + 1] = temp;
+
+          }
+
+       }
+    }
+}
+   void printArray(char array[], int size){
+  for( int i = 0; i < size; i ++){
+    printf("%c " , array[i]);
+  }
+}
+
+int main (){
+
+char array [] = {'h', 'c', 'n', 'm', 'a'};
+int size = sizeof(array)/sizeof(array[0]);
+  sort (array, size);
+  printArray(array, size);
+
+
+  return 0;
+}
