1a3630feea768821cc2dd8c8ee9c49c5fff3119b
aurelabode <lelabode@hotmail.com>
2025-12-12 20:45:03 +0100

    struct

--- ex2.c
+++ ex2.c
@@ -1,29 +1,33 @@
 # include <stdio.h>
-void sort(char array[] , int size){
-  for(int i = 0; i < size - 1; i ++){
-       for (int j = 0; j< size - i- 1; j ++){
-         if (array[j] > array[j + 1]){
+
+     void sort(char array[], int size){
+      for(int i = 0; i < size -1; i++){
+        for (int j = 0; j < size - i - 1; j ++){
+          if(array[j]>array[j+1]){
             char temp = array[j];
-                 array[j] = array[j + 1];
-                  array [j + 1] = temp;
-
+            array[j]= array[j+1];
+            array[j+1]= temp;
           }
-
-       }
-    }
-}
-   void printArray(char array[], int size){
-  for( int i = 0; i < size; i ++){
-    printf("%c " , array[i]);
+        }
+      }
+     }
+void printArray(char array[], int size){
+  for(int i = 0; i < size; i++){
+    printf("%c ", array[i]);
   }
 }
 
-int main (){
 
-char array [] = {'h', 'c', 'n', 'm', 'a'};
+
+
+
+int main(){
+
+char array[]= {'b','c','q','z','x','d','a','f','m'};
 int size = sizeof(array)/sizeof(array[0]);
-  sort (array, size);
-  printArray(array, size);
+sort(array,size);
+printArray(array,size);
+
 
 
   return 0;
