7e3f6ef339203899c15920a6b1b00e360bbfd3d7
aurelabode <lelabode@hotmail.com>
2025-12-13 17:12:04 +0100

    struct in arrays

---
+++ structarray.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+# include <string.h>
+
+struct Student{
+  char name[12];
+  float gpa;
+};
+
+
+int main(){
+
+struct Student student1={"bro", 9.5};
+struct Student student2 = {"Michael", 9.7};
+struct Student student3 = {"Tomas", 8};
+struct Student student4 = {"Vincent", 5};
+struct Student students[] = {student1, student2,student3, student4};
+for (int i = 0; i<sizeof(students)/sizeof(students[0]); i++){
+  printf("%-12s\t", students[i].name);
+  printf("%.2f\n", students[i].gpa);
+}
+
+
+  return 0;
+}
